b118c4528b2efa60ebe4bed36fde31bd3b56cfaf
kanuj365 <68420715+kanuj365@users.noreply.github.com>
2022-02-15 22:45:14 +0530

    Btnclick

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [flag, setflag] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button
+        id="click"
+        onClick={() => {
+          setflag(true);
+        }}
+      >
+        Click
+      </button>
+      {flag && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
